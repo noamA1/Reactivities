6ffb93daf497a613318a630614121f3dcac247b8
noam amron <noam0574@gmail.com>
2023-09-26 16:32:45 +0300

    prepered app for deploy

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -7,21 +7,29 @@ import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import TestErrors from "../../features/errors/TestError";
 import NotFound from "../../features/errors/NotFound";
 import ServerError from "../../features/errors/ServerError";
-import LoginForm from "../../features/users/LoginForm";
+// import LoginForm from "../../features/users/LoginForm";
 import ProfilePage from "../../features/profiles/ProfilePage";
+import RequireAurh from "./RequireAuth";
 
 export const routes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
     children: [
-      { path: "activities", element: <ActivityDashboard /> },
-      { path: "activities/:id", element: <ActivityDetails /> },
-      { path: "createActivity", element: <ActivityForm key="craate" /> },
-      { path: "manage/:id", element: <ActivityForm key="manage" /> },
-      { path: "profiles/:username", element: <ProfilePage /> },
-      { path: "login", element: <LoginForm key="manage" /> },
-      { path: "errors", element: <TestErrors /> },
+      {
+        element: <RequireAurh />,
+        children: [
+          { path: "activities", element: <ActivityDashboard /> },
+          { path: "activities/:id", element: <ActivityDetails /> },
+          { path: "createActivity", element: <ActivityForm key="craate" /> },
+          { path: "manage/:id", element: <ActivityForm key="manage" /> },
+          { path: "profiles/:username", element: <ProfilePage /> },
+          { path: "errors", element: <TestErrors /> },
+        ],
+      },
+
+      // { path: "login", element: <LoginForm key="manage" /> },
+
       { path: "not-found", element: <NotFound /> },
       { path: "server-error", element: <ServerError /> },
       { path: "*", element: <Navigate replace to="/not-found" /> },
